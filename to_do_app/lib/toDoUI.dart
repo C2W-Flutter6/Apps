b42c893a37ac916a681b813f4e5c3966332a664a
C2W-Flutter6 <flutterkhushi@gmail.com>
2024-07-12 18:29:23 +0530

    Modifying the showBottomSheet

--- to_do_app/lib/toDoUI.dart
+++ to_do_app/lib/toDoUI.dart
@@ -1,5 +1,85 @@
 //To Do App UI is further imporved in this code.
+//It consists of the Model class
 
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:intl/intl.dart';
+
+class ToDoApp extends StatefulWidget {
+  const ToDoApp({super.key});
+
+  @override
+  State createState() => _ToDoAppState();
+}
+
+class _ToDoAppState extends State {
+  TextEditingController titleController = TextEditingController();
+  TextEditingController dateController = TextEditingController();
+  TextEditingController descriptionController = TextEditingController();
+
+  void showBottomSheet(bool doEdit, [ToDoModelClass? toDoModelObj]) {
+    showModalBottomSheet(
+      isScrollControlled: true,
+      shape: const RoundedRectangleBorder(
+        borderRadius: BorderRadius.only(
+          topLeft: Radius.circular(30),
+          topRight: Radius.circular(30),
+        ),
+      ),
+      isDismissible: true,
+      context: context,
+      builder: (context) {
+        return Padding(
+          padding: EdgeInsets.only(
+            left: 20,
+            right: 20,
+            bottom: MediaQuery.of(context).viewInsets.bottom,
+          ),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              const SizedBox(
+                height: 12,
+              ),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Text(
+                    "Title",
+                    style: GoogleFonts.quicksand(
+                      color: const Color.fromRGBO(0, 139, 148, 1),
+                      fontWeight: FontWeight.w400,
+                      fontSize: 15,
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 3,
+                  ),
+                  TextField(
+                    controller: titleController,
+                    decoration: InputDecoration(
+                      focusedBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(12),
+                        borderSide: const BorderSide(
+                          color: Color.fromRGBO(0, 139, 148, 1),
+                        ),
+                      ),
+                      border: OutlineInputBorder(
+                        borderSide:
+                            const BorderSide(color: Colors.purpleAccent),
+                        borderRadius: BorderRadius.circular(12),
+                      ),
+                    ),
+                  ),
+                  const SizedBox(height: 12),
+                  Text(),
+                ],
+              ),
+            ],
+          ),
+        );
+      },
+    );
+  }
+}
