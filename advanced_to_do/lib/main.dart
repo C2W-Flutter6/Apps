d3ac298fd09ebd71007965a72d639bf5f9cc9b2a
C2W-Flutter6 <flutterkhushi@gmail.com>
2024-07-17 13:05:40 +0530

    Modified main.dart, created login and signup pages

--- advanced_to_do/lib/main.dart
+++ advanced_to_do/lib/main.dart
@@ -1,20 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:advanced_to_do/login_signup/login.dart';
+import 'package:advanced_to_do/login_signup/register.dart';
 
 void main() {
-  runApp(const MainApp());
-}
-
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
-    );
-  }
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: MyLogin(),
+    routes: {
+      'register': (context) => MyRegister(),
+      'login': (context) => MyLogin(),
+    },
+  ));
 }
