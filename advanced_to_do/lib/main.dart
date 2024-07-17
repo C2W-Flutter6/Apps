9f901f527c37a02e9f3d57a3431c09e03d0faeb0
C2W-Flutter6 <flutterkhushi@gmail.com>
2024-07-17 13:13:34 +0530

    Modified to import the third page

--- advanced_to_do/lib/main.dart
+++ advanced_to_do/lib/main.dart
@@ -1,14 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:advanced_to_do/login_signup/login.dart';
 import 'package:advanced_to_do/login_signup/register.dart';
+import 'ThirdPage.dart';
 
 void main() {
   runApp(MaterialApp(
     debugShowCheckedModeBanner: false,
-    home: MyLogin(),
+    home: const MyLogin(),
     routes: {
-      'register': (context) => MyRegister(),
-      'login': (context) => MyLogin(),
+      'register': (context) => const MyRegister(),
+      'login': (context) => const MyLogin(),
+      'home': (context) => const HomePage(),
     },
   ));
 }
